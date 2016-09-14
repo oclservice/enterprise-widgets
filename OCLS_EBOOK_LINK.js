2f90876e42269646be2cc8f673f144b890f99f67
Thomas Guignard <tom@timtom.ch>
2016-09-14 12:25:08 -0400

    Select college by drop down if no cookie value
    If the college is not specified in the cookie by the drop down menu in
    the header, the widget will display a modal window prompting the user to
    select a college before redirecting to the eBook link.
    For now, the widget does not attempt to set the cookie value.

--- OCLS_EBOOK_LINK.js
+++ OCLS_EBOOK_LINK.js
@@ -1,26 +1,98 @@
 function getEbookLink(rId){
-var wsBaseUrl="http://bibc.ocls.ca:8080/symws/";
-var clientId="DS_CLIENT";
-
-all_results=jQuery('#results_wrapper').find('.DOC_ID_value');
-
-var catKey;
-catKey=jQuery('#'+rId+'_DOC_ID').find('.DOC_ID_value').text().split(':')[1];
-var wsUrl=wsBaseUrl+'rest/standard/lookupTitleInfo?clientID='+clientId+'&titleID='+catKey+'&includeOPACInfo=false&marcEntryFilter=856&json=true';
-console.log("API call: "+wsUrl);
-var urlFrom856 = jQuery.ajax({
- type: 'GET',
- url: wsUrl,
- dataType: 'json',
- success: parse856Links,
- error: function (xhr, ajaxOptions, thrownError) {
-        console.log("API status: "+xhr.status);
-        console.log("API error: "+thrownError);
-      }
-});//end ajax()
+	// The 'targetCollege' global variable is set by the OCLS_COLLEGE_DROPDOWN widget
+	// Map this value to full college name as in the 856 entries
+	collegeNameMappings = { "algo": "Algonquin College", 
+						"bore": "Collège Boréal", 
+						"camb": "Cambrian College", 
+						"cent": "Centennial College",
+						"cone": "Conestoga College",
+						"conf": "Confederation College",
+						"durh": "Durham College",
+						"fans": "Fanshawe College",
+						"flem": "Fleming College",
+						"geob": "George Brown College",
+						"geog": "Georgian College",
+						"humb": "Humber College",
+						"laci": "La Cité collégiale",
+						"lamb": "Lambton College",
+						"loya": "Loyalist College",
+						"moha": "Mohawk College",
+						"niag": "Niagara College",
+						"nort": "Northern College",
+						"saul": "Sault College",
+						"sene": "Seneca College",
+						"sher": "Sheridan College",
+						"stcl": "St. Clair College",
+						"stla": "St. Lawrence College"
+						};
+	try{
+		var targetCollegeName = collegeNameMappings[targetCollege];
+	}
+	catch(e){
+		var targetCollegeName = "unknown";
+	}
+	
+	if ((typeof targetCollegeName == 'undefined')||(targetCollegeName=="unknown")){
+		// Mapping failed because targetCollege was not set, ask user to specify college.
+		// Build a drop down menu with all college names.
+		var collegeDropdown = document.createElement("select");
+		collegeDropdown.id = "ebookLinkCollegeSelector";
+		for (var college in collegeNameMappings){
+			var opt = document.createElement("option");
+			opt.value = collegeNameMappings[college];
+			var text = document.createTextNode(collegeNameMappings[college]);
+			opt.appendChild(text);
+			collegeDropdown.appendChild(opt);
+		};	
+		
+		console.log("Display drop down menu.")
+		
+		jQuery(function() {
+			jQuery(collegeDropdown).dialog({
+			width:500,
+			maxHeight:700,
+			title:'Please choose a college',
+			modal:true,
+			buttons:{
+				Done : function(){
+					get856Links(rId,this.options[this.selectedIndex].value);
+					jQuery(this).dialog("close");
+					}
+				}
+			});
+		});
+	}
+	else{
+		// targetCollege was set already, get the links and redirect
+		get856Links(rId,targetCollegeName);
+	}
+	
 }
 
-function parse856Links(sirsijson){
+function get856Links(rId,targetCollegeName){
+	var wsBaseUrl="http://bibc.ocls.ca:8080/symws/";
+	var clientId="DS_CLIENT";
+
+	all_results=jQuery('#results_wrapper').find('.DOC_ID_value');
+
+	var catKey;
+	catKey=jQuery('#'+rId+'_DOC_ID').find('.DOC_ID_value').text().split(':')[1];
+	var wsUrl=wsBaseUrl+'rest/standard/lookupTitleInfo?clientID='+clientId+'&titleID='+catKey+'&includeOPACInfo=false&marcEntryFilter=856&json=true';
+	console.log("API call: "+wsUrl);
+	var urlFrom856 = jQuery.ajax({
+	 type: 'GET',
+	 url: wsUrl,
+	 dataType: 'json',
+	 success: function(sirsijson){ parse856Links(sirsijson,targetCollegeName); },
+	 error: function (xhr, ajaxOptions, thrownError) {
+			console.log("API status: "+xhr.status);
+			console.log("API error: "+thrownError);
+		  }
+	});//end ajax()
+}
+
+
+function parse856Links(sirsijson,targetCollegeName){
 	// This function is called by jQuery.ajax with the results of the API call
 	console.log("API call successful");
 	
@@ -33,33 +105,6 @@ function parse856Links(sirsijson){
 	var urlTestSuffix='';
 	}
 	
-	// The 'targetCollege' global variable is set by the OCLS_COLLEGE_DROPDOWN widget
-	// Map this value to full college name as in the 856 entries
-	collegeNameMappings = { "algo": "Algonquin College", 
-						"bore": "Collège Boréal", 
-						"camb": "Cambrian College", 
-						"cent": "Centennial College",
-						"cone": "Conestoga College",
-						"conf": "Confederation College",
-						"durh": "Durham College",
-						"fans": "Fanshawe College",
-						"flem": "Fleming College",
-						"georb": "George Brown College",
-						"georg": "Georgian College",
-						"humb": "Humber College",
-						"lacite": "La Cité collégiale",
-						"lamb": "Lambton College",
-						"loya": "Loyalist College",
-						"moha": "Mohawk College",
-						"niag": "Niagara College",
-						"nort": "Northern College",
-						"saul": "Sault College",
-						"sene": "Seneca College",
-						"sher": "Sheridan College",
-						"stcl": "St. Clair College",
-						"stla": "St. Lawrence College"
-						}
-	var targetCollegeName = collegeNameMappings[targetCollege];
 	console.log("Target college: "+targetCollegeName);
 	var ebookUrl;
 	
